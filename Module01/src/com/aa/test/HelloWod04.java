d3b111b59fbb4b4c6f508835615552abc47a9566
user-cuizhihao <47172899+user-cuizhihao@users.noreply.github.com>
2020-08-19 02:46:18 +0800

    Update HelloWod04.java

--- Module01/src/com/aa/test/HelloWod04.java
+++ Module01/src/com/aa/test/HelloWod04.java
@@ -6,6 +6,6 @@ package com.aa.test;
  */
 public class HelloWod04 {
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("没有冲突制造冲突");
     }
 }
